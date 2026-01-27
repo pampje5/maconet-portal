d8b864cbfedec6743aec32f14bd1c8cbec3d10a7
pampje5 <info@roffeloac.nl>
2026-01-27 13:06:28 +0100

    Add Alembic for database migrations

---
+++ RoffelBackendPOC/alembic/versions/4f7df44aad7a_baseline.py
@@ -0,0 +1,28 @@
+"""baseline
+
+Revision ID: 4f7df44aad7a
+Revises: 
+Create Date: 2026-01-27 12:52:39.697884
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4f7df44aad7a'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
