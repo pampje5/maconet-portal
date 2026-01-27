2361ea28fb276bb574f59231aea81e760e2d0585
pampje5 <info@roffeloac.nl>
2026-01-27 17:39:20 +0100

    main open gezet

--- RoffelBackendPOC/app/main.py
+++ RoffelBackendPOC/app/main.py
@@ -26,7 +26,8 @@ from app.routers import (
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    # allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
