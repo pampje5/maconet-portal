e126bfd9ee402f837e67dfa63c8d40fd7327e776
jason <jasonroffel@hotmail.nl>
2026-01-28 21:56:22 +0100

    Nog meer hardcoded urls

--- roffel-portal/src/utils/auth.js
+++ roffel-portal/src/utils/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = process.env.REACT_APP_API_URL
 
 export async function getMe() {
   const token = localStorage.getItem("token");
